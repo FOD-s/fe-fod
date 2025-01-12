4235f15d89e2a8ffb9f65485fbd86f35f5542eab
ricky <rickyr1307@gmail.com>
2025-01-12 16:48:04 +0700

    bugfix: double cls

--- src/components/organisms/team-switcher.tsx
+++ src/components/organisms/team-switcher.tsx
@@ -24,24 +24,24 @@ export function TeamSwitcher({
     <SidebarMenu>
       <SidebarMenuItem>
         {/* <DropdownMenu> */}
-          {/* <DropdownMenuTrigger asChild> */}
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground hover:bg-bg-neumorphism "
-            >
-              <div className="flex items-center justify-center rounded-lg aspect-square size-8 text-sidebar-primary-foreground">
-                {/* <activeTeam.logo className="size-4" /> */}
-                <img src={Icon} className="size-8" alt="icons" className="rounded-sm"/>
-              </div>
-              <div className="grid flex-1 text-sm leading-tight text-left">
-                <span className="font-semibold truncate">
-                  PRIMA UTAMA
-                </span>
-                {/* <span className="text-xs truncate">{activeTeam.plan}</span> */}
-              </div>
-              {/* <ChevronsUpDown className="ml-auto" /> */}
-            </SidebarMenuButton>
-          {/* </DropdownMenuTrigger>
+        {/* <DropdownMenuTrigger asChild> */}
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground hover:bg-bg-neumorphism "
+        >
+          <div className="flex items-center justify-center rounded-lg aspect-square size-8 text-sidebar-primary-foreground">
+            {/* <activeTeam.logo className="size-4" /> */}
+            <img src={Icon} className="size-8 rounded-sm" alt="icons" />
+          </div>
+          <div className="grid flex-1 text-sm leading-tight text-left">
+            <span className="font-semibold truncate">
+              PRIMA UTAMA
+            </span>
+            {/* <span className="text-xs truncate">{activeTeam.plan}</span> */}
+          </div>
+          {/* <ChevronsUpDown className="ml-auto" /> */}
+        </SidebarMenuButton>
+        {/* </DropdownMenuTrigger>
           <DropdownMenuContent
             className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
             align="start"
