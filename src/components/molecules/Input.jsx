7d59102b98314a5b75874e9682090f9837e237aa
ricky <rickyr1307@gmail.com>
2025-01-13 16:28:00 +0700

    style : input note

--- src/components/molecules/Input.jsx
+++ src/components/molecules/Input.jsx
@@ -5,51 +5,51 @@ import { Label } from "@/components/ui/label";
 import { Controller } from "react-hook-form";
 
 const InputComponent = ({
-  name,
-  label,
-  type,
-  errors,
-  placeholder,
-  control,
-  disabled,
+	name,
+	label,
+	type,
+	errors,
+	placeholder,
+	control,
+	disabled,
 }) => {
-  return (
-    <div className="space-y-1">
-      <Label htmlFor={name} className="font-bold">
-        {label}
-      </Label>
-      <Controller
-        name={name}
-        control={control}
-        render={
-          label === "Alamat"
-            ? ({ field }) => (
-                <Textarea
-                  id={name}
-                  name={name}
-                  type={type}
-                  placeholder={placeholder}
-                  disabled={disabled}
-                  {...field}
-                  className="bg-bg-neumorphism !shadow-neumorphism-inner"
-                />
-              )
-            : ({ field }) => (
-                <Input
-                  id={name}
-                  name={name}
-                  type={type}
-                  placeholder={placeholder}
-                  disabled={disabled}
-                  {...field}
-                  className="bg-bg-neumorphism !shadow-neumorphism-inner"
-                />
-              )
-        }
-      />
-      {errors[name] && <ErrorInputForm message={errors[name].message} />}
-    </div>
-  );
+	return (
+		<div className="space-y-1">
+			<Label htmlFor={name} className="font-bold">
+				{label}
+			</Label>
+			<Controller
+				name={name}
+				control={control}
+				render={
+					label === "Alamat" || label === "Note"
+						? ({ field }) => (
+								<Textarea
+									id={name}
+									name={name}
+									type={type}
+									placeholder={placeholder}
+									disabled={disabled}
+									{...field}
+									className="bg-bg-neumorphism !shadow-neumorphism-inner"
+								/>
+						  )
+						: ({ field }) => (
+								<Input
+									id={name}
+									name={name}
+									type={type}
+									placeholder={placeholder}
+									disabled={disabled}
+									{...field}
+									className="bg-bg-neumorphism !shadow-neumorphism-inner"
+								/>
+						  )
+				}
+			/>
+			{errors[name] && <ErrorInputForm message={errors[name].message} />}
+		</div>
+	);
 };
 
 export default InputComponent;
