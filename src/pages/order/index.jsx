c62bcaca3ddc46462d33b0d5960dda22cc4c707e
ricky <rickyr1307@gmail.com>
2025-01-21 00:04:01 +0700

    test deploy

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -103,8 +103,8 @@ function Order() {
     type: "add",
   });
   const { getTrundleBedPrice } = useTrundleBedService();
-  const [pageForm, setPageForm] = useState(true);
-  const [openCollapse, setOpenCollapse] = useState(true);
+  const [pageForm, setPageForm] = useState(false);
+  const [openCollapse, setOpenCollapse] = useState(false);
   const user = useSelector(DATA_USER);
   const { toast } = useToast();
 
